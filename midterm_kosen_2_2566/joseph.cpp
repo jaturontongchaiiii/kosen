f84b7fd9d68bf255fa0b260b89bb9a45e4d80211
tongchai <jaturontongchaiiii@gmail.com>
2023-12-08 11:01:31 +0700

    ีupdate name survivor

--- midterm_kosen_2_2566/joseph.cpp
+++ midterm_kosen_2_2566/joseph.cpp
@@ -11,8 +11,8 @@ int main() {
     int countPeople = 16;
     string* people = new string [countPeople];
     people[0] = "Olivia"; people[1] = "Emma"; people[2] = "Hannah"; people[3] = "Vida"; 
-    people[4] = "Milada"; people[5] = "Felicity"; people[6] = "Vida"; people[7] = "Amada"; 
-    people[8] = "Jamille"; people[9] = "Lana"; people[10] = "Joseph"; people[11] = "Belinda"; 
+    people[4] = "Milada"; people[5] = "Felicity"; people[6] = "Joseph"; people[7] = "Amada"; 
+    people[8] = "Jamille"; people[9] = "Lana"; people[10] = "Julia"; people[11] = "Belinda"; 
     people[12] = "Mabel"; people[13] = "Naomi"; people[14] = "Amity"; people[15] = "Gabriella";
 
     int die = 0;
